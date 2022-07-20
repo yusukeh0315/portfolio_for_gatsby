cf38c8bd6ba4cddc843c407a92d915c846d832ca
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2022-07-20 18:39:51 +0900

    jsテスト

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
 import React from "react"
-import { Helmet } from "react-helmet"
-import { withPrefix } from "gatsby"
+// import { Helmet } from "react-helmet"
+// import { withPrefix } from "gatsby"
 
 import Header from "../components/header"
 import Footer from "../components/footer"
+import Javascript from "../components/javascript"
 
 import "./layout.css"
 
@@ -14,13 +15,14 @@ const Layout = ({ children }) => (
         {children}
 
         <Footer />
-        <Helmet>
+        <Javascript />
+        {/* <Helmet>
             <script src={withPrefix('https://code.jquery.com/jquery-3.4.1.min.js')} type="text/javascript" />
             <script src={withPrefix('https://cdn.jsdelivr.net/npm/shuffle-text@0.3.0/build/shuffle-text.min.js')} type="text/javascript" />
             <script src={withPrefix('https://cdnjs.cloudflare.com/ajax/libs/vivus/0.4.4/vivus.min.js')} type="text/javascript" />
             <script src={withPrefix('https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js')} type="text/javascript" />
             <script src={withPrefix('/javascripts/main.js')} type="text/javascript" />
-        </Helmet>
+        </Helmet> */}
     </div>
 
 )
