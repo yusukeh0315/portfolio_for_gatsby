683f1801b4955292a3d5dd0a6cb9f3c4f24399ca
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2022-07-22 10:24:50 +0900

    microCMSを使うための設定追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,11 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+//環境変数を使うために必要
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -39,5 +44,20 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-source-microcms",
+      options: {
+        apiKey: process.env.microCMS_API_KEY.replace(/\"/g, ''),
+        serviceId: "vit-works",
+        apis: [
+          {
+            endpoint: "works",
+          },
+          {
+            endpoint: "skills",
+          },
+        ],
+      },
+    },
   ],
 }
