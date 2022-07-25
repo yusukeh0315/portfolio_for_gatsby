7d49f4ad035442e6e7c0f6626b8908039fc4b23f
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2022-07-25 10:55:37 +0900

    問い合わせ修正

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,24 +19,15 @@ const Contact = ({ data, location }) => (
           <div className="p-section__title-wrapper">
             <h2 className="p-section__title js_typing">Contact</h2>
           </div>
-          {/* /.section__title-wrapper*/}
+
           <div className="p-contact__lead-wrapper">
-            <p className="p-contact__lead">制作の依頼、ご質問などお問い合わせください。<br />                    下記のフォームより、記入お願いします。</p>
+            <p className="p-contact__lead">
+              制作の依頼、ご質問などお問い合わせください。<br />
+              下記のフォームより、記入お願いします。
+            </p>
           </div>
-          {/* /.contact__lead-wrapper*/}
-          <form method="post"
-                action="https://www.flexyform.com/f/6b68836a9cdc6e7f2a5f1f7d39e6b4ac8ba8c6cd">
-              <p>
-                  <label>Fullname</label>
-                  <input type="text" name="fullname" />
-              </p>
-              <p>
-                  <label>Email</label>
-                  <input type="email" name="_reply_to" />
-              </p>
-              <button type="submit">Send</button>
-          </form>
-          {/* <div className="p-contact__main">
+
+          <div className="p-contact__main">
             <form className="p-contact__form" id="form" method="post" action="https://www.flexyform.com/f/6b68836a9cdc6e7f2a5f1f7d39e6b4ac8ba8c6cd">
               <div className="p-contact__item">
                 <label className="p-contact__label" htmlFor="name">氏名<span className="c-require-label">必須</span></label>
@@ -58,13 +49,9 @@ const Contact = ({ data, location }) => (
                 <label className="p-contact__label" htmlFor="message">お問い合わせ内容<span className="c-require-label">必須</span></label>
                 <textarea className="p-contact__message" id="message" name="entry.195281403" placeholder="お問い合わせ内容" defaultValue={""} />
               </div>
-              <input className="c-submit-button" id="js-submit" disabled type="submit" />
+              <button className="c-submit-button" type="submit">送信</button>
             </form>
-            <p className="end-message contact-main__end-message">お問い合わせありがとうございました。</p>
-            <p className="false-message contact-main__false-message">送信失敗しました。</p>
-          </div> */}
-          {/* /.p-contact__main*/}
-          {/* /.p-contact__inner*/}
+          </div>
         </div>
       </section>
 
