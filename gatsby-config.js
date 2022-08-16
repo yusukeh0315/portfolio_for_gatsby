8ebaea94922cd444ef2cd43c29d8a6920f0ccbc8
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2022-08-16 16:46:07 +0900

    sitemapのディレクトリの設定変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-sitemap",
       options: {
-        output: `/sitemap.xlm`,
+        output: `/sitemap`,
         createLinkInHead: true,
       },
     },
