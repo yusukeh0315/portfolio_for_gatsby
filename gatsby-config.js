f99a5ba9de1a22bebe646551a4be8cda1c3d90be
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2022-08-16 15:32:21 +0900

    siteUrlの更新

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     title: `VIT Works`,
     description: `ITを活用した様々なソリューションを提供するVIT Worksのサイト`,
     lang: `ja`,
-    siteUrl: `https://deluxe-cobbler-90513b.netlify.app/`,
+    siteUrl: `https://vitdata.net/`,
     local: `ja_JP`,
     fbappid: `2139031849601255`,
   },
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: "gatsby-source-microcms",
       options: {
-        apiKey: process.env.microCMS_API_KEY.replace(/\"/g, ''),
+        apiKey: process.env.microCMS_API_KEY.replace(/\"/g, ""),
         serviceId: "vit-works",
         apis: [
           {
@@ -62,5 +62,32 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
     `body-scroll-lock`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              siteUrl
+            }
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+          }
+        }`,
+        serialize: ({ site, allSitePage }) => {
+          return allSitePage.nodes.map(node => {
+            return {
+              url: `${site.siteMetadata.siteUrl}${node.path}`,
+              changefreq: `weekly`,
+              priority: 0.5,
+            }
+          })
+        }
+      }
+    },
   ],
 }
