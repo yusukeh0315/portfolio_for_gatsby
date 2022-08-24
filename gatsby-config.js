61fe3f66a59f203f03b37c6bf99aad7258d056ed
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2022-08-24 15:51:34 +0900

    Deletion of subtree-applicable directories

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,6 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images/`,
-    //   },
-    // },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
