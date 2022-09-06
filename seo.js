0ed45ad91aab40ad21f7db82214831c9598f8cd7
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2022-09-06 15:04:52 +0900

    Squashed 'src/components/' changes from b7fc1fc..44fecb0
    44fecb0 構造化データの修正
    0f1fc86 構造化データの追加
    4a1899e mod header
    
    git-subtree-dir: src/components
    git-subtree-split: 44fecb04ec6a72de2cabf9c773ca28f52470c1b4

--- seo.js
+++ seo.js
@@ -3,67 +3,125 @@ import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Seo = props => {
-    const data = useStaticQuery(graphql`
-        query {
-            site {
-                siteMetadata {
-                    title
-                    lang
-                    description
-                    siteUrl
-                    local
-                    fbappid
-                }
-            }
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          lang
+          description
+          siteUrl
+          local
+          fbappid
         }
-    `)
-
-    let title = props.pagetitle
-        ? `${props.pagetitle} | ${data.site.siteMetadata.title}`
-        : data.site.siteMetadata.title
-
-    if (props.pagetitle === "Portfolio") {
-        title = props.pagetitle
+      }
     }
+  `)
 
-    const description = props.pagedesc || data.site.siteMetadata.description
+  let title = props.pagetitle
+    ? `${props.pagetitle} | ${data.site.siteMetadata.title}`
+    : data.site.siteMetadata.title
 
-    const url = props.pagepath
-        ? `${data.site.siteMetadata.siteUrl}${props.pagepath}`
-        : data.site.siteMetadata.siteUrl
+  if (props.pagetitle === "Portfolio") {
+    title = props.pagetitle
+  }
 
-    const imgurl = props.pageimg
-      ? `${data.site.siteMetadata.siteUrl}${props.pageimg}`
-      : props.blogimg || `${data.site.siteMetadata.siteUrl}/ogp_image.png`
-    const imgw = props.pageimgw || 1280
-    const imgh = props.pageimgh || 640
+  const description = props.pagedesc || data.site.siteMetadata.description
 
-    return (
-        <div>
-            <Helmet>
-                <html lang={data.site.siteMetadata.lang} />
-                <title>{title}</title>
-                <meta name="description" content={description} />
+  const url = props.pagepath
+    ? `${data.site.siteMetadata.siteUrl}${props.pagepath}`
+    : data.site.siteMetadata.siteUrl
 
-                <link rel="canonical" href={url} />
+  const imgurl = props.pageimg
+    ? `${data.site.siteMetadata.siteUrl}${props.pageimg}`
+    : props.blogimg || `${data.site.siteMetadata.siteUrl}/ogp_image.png`
+  const imgw = props.pageimgw || 1280
+  const imgh = props.pageimgh || 640
 
-                <meta property="og:site_name" content={data.site.siteMetadata.title} />
-                <meta property="og:title" content={title} />
-                <meta property="og:description" content={description} />
-                <meta property="og:url" content={url} />
-                <meta property="og:type" content="website" />
-                <meta property="og:locale" content={data.site.siteMetadata.locale} />
-                <meta property="fb:app_id" content={data.site.siteMetadata.fbappid} />
 
-                <meta property="og:image" content={imgurl} />
-                <meta property="og:image:width" content={imgw} />
-                <meta property="og:image:height" content={imgh} />
-                <meta name="twitter:card" content="summary_large_image" />
+  let itemListContents = []
+  if (`${data.site.siteMetadata.siteUrl}` === "https://vitworks.net/") {
+    itemListContents = [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "VIT Works",
+        item: `${data.site.siteMetadata.siteUrl}`,
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "About",
+        item: `${data.site.siteMetadata.siteUrl}/about/`,
+      },
+      {
+        "@type": "ListItem",
+        position: 3,
+        name: "Works",
+        item: `${data.site.siteMetadata.siteUrl}/works/`,
+      },
+      {
+        "@type": "ListItem",
+        position: 4,
+        name: "Contact",
+        item: `${data.site.siteMetadata.siteUrl}/contact/`,
+      },
+    ]
+  } else {
+    itemListContents = [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "VIT Works",
+        item: `${data.site.siteMetadata.siteUrl}`,
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "About",
+        item: `${data.site.siteMetadata.siteUrl}/about/`,
+      },
+      {
+        "@type": "ListItem",
+        position: 3,
+        name: "Works",
+        item: `${data.site.siteMetadata.siteUrl}/works/`,
+      },
+    ]
+  }
 
-            </Helmet>
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: `${itemListContents}`,
+  }
 
-        </div>
-    )
+  return (
+    <div>
+      <Helmet>
+        <html lang={data.site.siteMetadata.lang} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <link rel="canonical" href={url} />
+
+        <meta property="og:site_name" content={data.site.siteMetadata.title} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content={data.site.siteMetadata.locale} />
+        <meta property="fb:app_id" content={data.site.siteMetadata.fbappid} />
+
+        <meta property="og:image" content={imgurl} />
+        <meta property="og:image:width" content={imgw} />
+        <meta property="og:image:height" content={imgh} />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        {<script type="application/ld+json">{JSON.stringify(jsonLd)}</script>}
+      </Helmet>
+    </div>
+  )
 }
 
 export default Seo
