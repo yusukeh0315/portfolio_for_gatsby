70835b51ee7395ff3d8a7fc1ad7fa5199f1acd62
yusuke.h0315@gmail.com <yusuke.h0315@gmail.com>
2023-01-12 09:03:32 +0900

    Squashed 'src/components/' changes from 3980f0a..8e48e76
    8e48e76 バリデーションの修正
    b5ca39a 不要なimportを削除
    
    git-subtree-dir: src/components
    git-subtree-split: 8e48e763dad47e4b100b1cd4007aa71dd821915e

--- estimateradio.js
+++ estimateradio.js
@@ -5,6 +5,7 @@ import RadioGroup from "@mui/material/RadioGroup"
 import FormControlLabel from "@mui/material/FormControlLabel"
 import FormControl from "@mui/material/FormControl"
 import FormLabel from "@mui/material/FormLabel"
+import FormHelperText from "@mui/material/FormHelperText"
 
 import { EstimateContext } from "../pages/estimate"
 import { PageContext } from "../pages/estimate"
@@ -13,15 +14,21 @@ import { UnitContext } from "../pages/estimate"
 import { DesignUnitContext } from "../pages/estimate"
 import { WebContext } from "../pages/estimate"
 // import { DesignContext } from "../pages/estimate"
+import { CmsContext } from "../pages/estimate"
 
 const EstimateRadio = ({ question_title, inputQuestions }) => {
-  const methods = useFormContext()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useFormContext()
   const [value, setValue] = useContext(EstimateContext)
   const [, setPage] = useContext(PageContext)
   const [type, setType] = useContext(TypeContext)
   const [, setUnit] = useContext(UnitContext)
   const [, setDesignUnit] = useContext(DesignUnitContext)
   const [webValue, setWebValue] = useContext(WebContext)
+  const [cmsValue, setCmsValue] = useContext(CmsContext)
   // const [designValue, setDesignValue] = useContext(DesignContext)
   const [currentValue, setCurrentValue] = useState(0)
   const [question, setQuestion] = useState(inputQuestions)
@@ -56,32 +63,41 @@ const EstimateRadio = ({ question_title, inputQuestions }) => {
       }
     }
 
-    if (
-      question_title === "デザイン案はありますか？"
-    ) {
+    if (question_title === "デザイン案はありますか？") {
       if (selectLabel === "いいえ") {
-        setDesignUnit(0)
+        setDesignUnit(1)
       } else if (
         selectLabel ===
         "はい（イメージ、参考サイト、ワイヤーフレームなどはある）"
       ) {
         setDesignUnit(0.8)
       } else if (selectLabel === "はい（デザインの作成はない）") {
-        setDesignUnit(1.0)
+        setDesignUnit(0)
       } else if (selectLabel === "おまかせ") {
-        setDesignUnit(1.0)
+        setDesignUnit(0)
+      }
+    }
+
+    if (
+      question_title ===
+      "投稿機能、更新する項目、頻度はどのくらいですか？（CMSの導入、選定の参考にします）"
+    ) {
+      if (selectLabel === "投稿、更新はしない（CMSを使用しない）") {
+        setCmsValue(0)
+      } else if (selectLabel === "少ない（WordPress以外のCMSを使用する）") {
+        setCmsValue(1)
+      } else if (selectLabel === "多い（WordPressを使用する）") {
+        setCmsValue(2)
       }
     }
 
     const addValue = selectValue - currentValue
 
     if (
-      (question_title === "ログイン機能は必要ですか？" ||
-        question_title === "会員登録・顧客管理を行いますか？" ||
-        question_title === "決済できる機能は必要ですか？" ||
-        question_title === "検索する機能は必要ですか？" ||
-        question_title ===
-          "更新する項目、頻度はどのくらいですか？（CMSの導入、選定の参考にします）")
+      question_title === "ログイン機能は必要ですか？" ||
+      question_title === "会員登録・顧客管理を行いますか？" ||
+      question_title === "決済できる機能は必要ですか？" ||
+      question_title === "検索する機能は必要ですか？"
     ) {
       setWebValue(webValue + addValue)
       setCurrentValue(selectValue)
@@ -90,15 +106,17 @@ const EstimateRadio = ({ question_title, inputQuestions }) => {
       setCurrentValue(selectValue)
     }
 
-
-
     if (question_title === "どのくらいページを作成しますか？*") {
       if (selectLabel === "１～３") {
         setPage(3)
-      } else if (selectLabel === "４～９") {
-        setPage(9)
-      } else if (selectLabel === "１０～４９") {
-        setPage(50)
+      } else if (selectLabel === "４～６") {
+        setPage(6)
+      } else if (selectLabel === "６～１０") {
+        setPage(10)
+      } else if (selectLabel === "１１～２０") {
+        setPage(20)
+      } else if (selectLabel === "２１～３０") {
+        setPage(30)
       } else {
         setPage(100)
       }
@@ -115,10 +133,9 @@ const EstimateRadio = ({ question_title, inputQuestions }) => {
           row
           aria-labelledby="demo-row-radio-buttons-group-label"
           name="row-radio-buttons-group"
-          {...methods.register(`${question_title}`)}
+          {...register(`${question_title}`)}
         >
           {inputQuestions.map(value => {
-
             let disable = false
             if (
               (question_title === "ログイン機能は必要ですか？" ||
@@ -126,7 +143,7 @@ const EstimateRadio = ({ question_title, inputQuestions }) => {
                 question_title === "決済できる機能は必要ですか？" ||
                 question_title === "検索する機能は必要ですか？" ||
                 question_title ===
-                  "更新する項目、頻度はどのくらいですか？（CMSの導入、選定の参考にします）") &&
+                  "投稿機能、更新する項目、頻度はどのくらいですか？（CMSの導入、選定の参考にします）") &&
               type === 1
             ) {
               disable = true
@@ -138,23 +155,51 @@ const EstimateRadio = ({ question_title, inputQuestions }) => {
             } else {
               disable = false
             }
-            return (
-              <FormControlLabel
-                key={value.label}
-                value={value.label}
-                disabled={disable}
-                control={
-                  <Radio
-                    checked={disable ? false : question === value.label}
-                    onChange={onChange}
-                  />
-                }
-                label={value.label}
-                {...methods.register(`${question_title}`)}
-              />
-            )
+
+            // HACK 質問によってreturnの結果を返している
+            // requiredを変数で指定してもすべて必須で反映されてしまう。
+            if (
+              question_title === "依頼したい内容は何ですか？*" ||
+              question_title === "新規に作成しますか？修正・更新しますか？*" ||
+              question_title === "どのくらいページを作成しますか？*"
+            ) {
+              return (
+                <FormControlLabel
+                  key={value.label}
+                  value={value.label}
+                  disabled={disable}
+                  control={
+                    <Radio
+                      checked={disable ? false : question === value.label}
+                      onChange={onChange}
+                    />
+                  }
+                  label={value.label}
+                  {...register(`${question_title}`, { required: true })}
+                />
+              )
+            } else {
+              return (
+                <FormControlLabel
+                  key={value.label}
+                  value={value.label}
+                  disabled={disable}
+                  control={
+                    <Radio
+                      checked={disable ? false : question === value.label}
+                      onChange={onChange}
+                    />
+                  }
+                  label={value.label}
+                  {...register(`${question_title}`)}
+                />
+              )
+            }
           })}
         </RadioGroup>
+        <FormHelperText error={`${question_title}` in errors}>
+          {`${question_title}` in errors && "入力してください"}
+        </FormHelperText>
       </FormControl>
     </>
   )
